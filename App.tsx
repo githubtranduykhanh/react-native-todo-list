5c1d74358bfbc785dec2d5549a321f86f54967ac
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-08-10 18:00:33 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { fontFamilies, colors } from './src/constants';
+
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    [fontFamilies.regular]: require('./assets/fonts/Poppins-Regular.ttf'),
+    [fontFamilies.medium]: require('./assets/fonts/Poppins-Medium.ttf'),
+    [fontFamilies.semiBold]: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    [fontFamilies.bold]: require('./assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: fontFamilies.bold, fontSize: 20, color:colors.test }}>Roboto Regular</Text>
+      <Text style={{ fontFamily: fontFamilies.medium, fontSize: 20 ,color:colors.test}}>Roboto Bold</Text>
     </View>
   );
 }
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.bgColor,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center',    
   },
 });
